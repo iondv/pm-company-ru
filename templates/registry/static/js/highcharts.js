bb342389f25f8ebc37f1a32e71a6ece3e092d048
nastya <nastena.piskunova@inbox.ru>
2021-03-16 16:49:05 +1000

    редактирование метаданных

--- templates/registry/static/js/highcharts.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-if (!window.highchartReady) {
-  window.highchartReady = true;
-  $.getScript('registry/app-vendor/highcharts/js/highcharts.js', function () {
-    $(document.body).trigger('highcharts:ready');
-  });
-}
